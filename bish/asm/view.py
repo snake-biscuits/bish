8470e32ae24a03042f3658eeca904beceaaa97bc
snake-biscuits <36507175+snake-biscuits@users.noreply.github.com>
2026-01-14 22:16:27 +1000

    (asm.view) calculate `sub_offset` for multi-line instructions

--- bish/asm/view.py
+++ bish/asm/view.py
@@ -3,13 +3,18 @@
 # -- instruction segments
 
 
-def r2(fxc, limit=None):
+def r2(fxc, limit=None, start=0):
     """radare 2 inspired assembly bytecode viewer"""
     assert "SHEX" in fxc.chunks
     # 0000 | version & shader type token
     # 0004 | chunk size
     offset = 8  # first instruction
-    for i, instruction in enumerate(fxc.SHEX.instructions[:limit]):
+    offset += sum(
+        len(instruction) * 4
+        for instruction in fxc.SHEX.instructions[:start])
+    limit = start + limit if limit is not None else None
+    for i, instruction in enumerate(fxc.SHEX.instructions[start:limit]):
+        i = start + i
         token_length = len(instruction)
         length = token_length * 4
         # raw instruction bytes
@@ -31,8 +36,9 @@ def r2(fxc, limit=None):
         else:  # multi-line
             for j in range((token_length + 3) // 4):
                 sub_hex_ = hex_[j*36:(j+1)*36-1]
+                sub_offset = offset + j * 16
                 i_s = f"{i:4}" if j == 0 else " " * 4
                 tail = f"{opcode} ..." if j == 0 else ""
-                print(f"{i_s} | {offset:04X} | {sub_hex_} | {tail}")
+                print(f"{i_s} | {sub_offset:04X} | {sub_hex_} | {tail}")
 
         offset += length
